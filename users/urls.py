b63a2ec3379cfee5e2018c1d146fcf7c1e38262f
youngroma <krivichroma228@gmail.com>
2025-02-26 18:04:00 +0100

    added urls for handling password

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterUser, LoginUser, ReferralLink, ReferralStats, ForgotPassword, ResetPassword
+from .views import RegisterUser, LoginUser, ForgotPassword, ResetPassword
 
 urlpatterns = [
     path('register/', RegisterUser.as_view(), name='register'),
     path('login/', LoginUser.as_view(), name='login'),
-
+    path('forgot-password/', ForgotPassword.as_view(), name='forgot-password'),
+    path('reset-password/<str:token>/', ResetPassword.as_view(), name='reset-password'),
 ]
 
