f7ff39c518765bd445d16295a0296183c1703e73
youngroma <krivichroma228@gmail.com>
2025-03-08 15:25:38 +0100

    Changed: use the container name for redis

--- platform_similar_to_linktree/settings.py
+++ platform_similar_to_linktree/settings.py
@@ -154,7 +154,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',  # Assuming Redis is running locally
+        'LOCATION': 'redis://redis:6379/1',
     }
 }
 
